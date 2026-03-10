344ef0e35215771f49a31c7b17b125caaa7bf37d
Hitesh Malviya <hiteshmalviya955@gmail.com>
2026-03-10 09:00:37 +0530

    Collection Practice

---
+++ PriorityQueue/P1.java
@@ -0,0 +1,64 @@
+package PriorityQueue;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class P1 {
+	public static void main(String[] args) {
+		// there two type of ArrayList 
+		// homogenous type & Heterogenous Type
+        // ArrayList basically extend abstractlist and implement List
+		// In arraylist Duplicate element Allow
+		// there 
+//		List al1 = new ArrayList();
+//		al1.add(10);
+//		al1.add(20);
+//		al1.add(30);
+//		
+//		
+//		List al = new ArrayList();
+//		al.add(10);
+//		al.add(20);
+//		al.add(30);
+//		al.add(al1);
+//		System.out.println(al.get(1));
+//		System.out.println(al);
+		
+		// LinkedList 
+		// duplicate Elements Allow
+		// LinkedList implement list and Deque
+		// Internally Linked List follow Doubly linked list
+		// Insertion order and Add Order
+		
+		List ll = new LinkedList();
+		ll.add(10);
+		ll.add(20);
+		ll.add(30);
+		
+	    // queue interface
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+//		Queue pq = new PriorityQueue();
+//	    pq.offer(30);
+//	    pq.offer(40);
+//	    pq.offer(20);
+//	    pq.offer(50);
+//	    
+//	    System.out.println(pq);
+//	    pq.poll();
+//	    System.out.println(pq);
+//	    System.out.println(pq.peek());
+	}
+
+}
